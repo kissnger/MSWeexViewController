446fc4556d17124c83441d3d45faa1ac91def54b
Massimo <16707587@qq.com>
2017-01-16 14:56:18 +0800

    no message

---
+++ MSWeexViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MSWeexViewController
+//
+//  Created by Massimo on 2017/1/16.
+//  Copyright © 2017年 Massimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
